1c92d0c52bcdd373ceba07c809262cfc193c33b6
Yulia Ioffe <leanor@yandex.ru>
2016-07-23 14:31:36 +0300

    Revert "Добавили уравнение площади и разные типы переменных"
    This reverts commit 804bc6128fff3542ac9f6145e74f6600056bc66a.

--- Sandbox/src/main/java/leanor/java_qa/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/leanor/java_qa/sandbox/MyFirstProgram.java
@@ -4,17 +4,7 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 
-		String somebody = "world";
-		System.out.println("Hello, " + somebody + "!");
-    System.out.println();
-
-    double l = 6;
-		double s = l * l;
-
-
-		System.out.println("площадь квадрата со стороной " + l + " = " + s);
-
-
+		System.out.println("Hello, world!");
 	}
 
 }
