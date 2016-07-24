552964bbcce161f2c458c4b1abab1236fdd1f643
Yulia Ioffe <leanor@yandex.ru>
2016-07-24 15:48:40 +0300

    Добавлена библиотека для тестов и написан первый тест

---
+++ Sandbox/src/test/java/leanor/java_qa/sandbox/SquareTests.java
@@ -0,0 +1,16 @@
+package leanor.java_qa.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by leanor on 24.07.2016.
+ */
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25.0);
+  }
+}
