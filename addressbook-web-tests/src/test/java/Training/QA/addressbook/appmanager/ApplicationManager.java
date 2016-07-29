87e0dd2b1442bd62b3c9b527766634865aa3f697
Yulia Ioffe <leanor@yandex.ru>
2016-07-29 13:52:01 +0300

    NavigationHelper created

--- addressbook-web-tests/src/test/java/Training/QA/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/Training/QA/addressbook/appmanager/ApplicationManager.java
@@ -13,6 +13,7 @@ public class ApplicationManager {
 
   FirefoxDriver wd;
 
+  private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
@@ -28,11 +29,12 @@ public class ApplicationManager {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    wd.get("http://localhost/addressbook/");
     login("admin", "secret");
   }
 
   public void login(String username, String password) {
-    wd.get("http://localhost/addressbook/");
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -43,15 +45,15 @@ public class ApplicationManager {
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
-  public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
-
   public void stop() {
     wd.quit();
   }
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
   }
 }
