925e5f974c5851851f410f18f37baa5f61021a46
Yulia Ioffe <leanor@yandex.ru>
2016-07-29 15:42:05 +0300

    ContactModification added

--- addressbook-web-tests/src/test/java/Training/QA/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/Training/QA/addressbook/appmanager/ContactHelper.java
@@ -41,4 +41,11 @@ public class ContactHelper extends HelperBase {
   }
 
 
+  public void initContactModification() {
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void confirmContactModification() {
+    click(By.xpath("//div[@id='content']/form[1]/input[22]"));
+  }
 }
---
+++ addressbook-web-tests/src/test/java/Training/QA/addressbook/tests/ContactModificationTest.java
@@ -0,0 +1,23 @@
+package Training.QA.addressbook.tests;
+
+import Training.QA.addressbook.model.ContactData;
+import org.testng.annotations.Test;
+
+/**
+ * Created by leanor on 29.07.2016.
+ */
+public class ContactModificationTest extends TestBase {
+
+  @Test
+  public void testContactModification(){
+    app.getNavigationHelper().gotoHome();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactData("NameTestMODIF", "LastNameTest",
+            "testtest", "test", "CompTets", "Address test test", "5849395", "+93927298403",
+            "95040843", "ttt@kmkmk.tt"));
+    app.getContactHelper().confirmContactModification();
+    app.getNavigationHelper().gotoHome();
+
+  }
+
+}
