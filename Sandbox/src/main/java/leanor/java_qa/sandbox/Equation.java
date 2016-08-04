232ce5186373ec1eee36118ef7651612ac6a8f4d
Yulia Ioffe <leanor@yandex.ru>
2016-08-04 13:23:40 +0300

    Added If Else and optimized

---
+++ Sandbox/src/main/java/leanor/java_qa/sandbox/Equation.java
@@ -0,0 +1,52 @@
+package leanor.java_qa.sandbox;
+
+/**
+ * Created by leanor on 31.07.2016.
+ */
+public class Equation {
+
+  private double a;
+  private double b;
+  private double c;
+
+  private int n;
+
+  public Equation(double a, double b, double c) {
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    double d = b * b - 4 * a * c;
+
+    if (a != 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
+        n = 1;
+      } else {
+        n = 0;
+      }
+    } else if (b != 0) {
+      System.out.println("Вырожденное уравнение");
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n = -1;
+    }
+
+      }
+
+    
+
+
+
+
+
+  public int rootNumber() {
+    return n;
+  }
+}
