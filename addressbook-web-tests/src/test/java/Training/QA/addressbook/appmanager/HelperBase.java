7738c00d6150472a4e2d11c025fa19b1c31c83e7
Yulia Ioffe <leanor@yandex.ru>
2016-08-05 13:24:49 +0300

    Added option to change browser type
    as a parameter to ApplicationManager. Parameter value should be changed
    in TestBase.

--- addressbook-web-tests/src/test/java/Training/QA/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/Training/QA/addressbook/appmanager/HelperBase.java
@@ -2,15 +2,16 @@ package Training.QA.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by leanor on 29.07.2016.
  */
 public class HelperBase {
-  protected FirefoxDriver wd;
+  protected WebDriver wd;
 
-  public HelperBase(FirefoxDriver wd) {
+  public HelperBase(WebDriver wd) {
     this.wd = wd;
   }
 
--- addressbook-web-tests/src/test/java/Training/QA/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/Training/QA/addressbook/tests/TestBase.java
@@ -1,6 +1,7 @@
 package Training.QA.addressbook.tests;
 
 import Training.QA.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -9,7 +10,7 @@ import org.testng.annotations.BeforeMethod;
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod
   public void setUp() throws Exception {
