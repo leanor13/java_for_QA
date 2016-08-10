a621d60e86681d7ed835d8996fe8198f806793fc
Yulia Ioffe <leanor@yandex.ru>
2016-08-10 17:17:03 +0300

    checking list of modified groups

--- addressbook-web-tests/src/test/java/Training/QA/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/Training/QA/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import Training.QA.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -20,11 +21,18 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectCheckbox(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("Test1", "TestsBEBWBEB", "Test5555"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Test8", "TestsBEBWBEB", "Test5555");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+
+
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
 
   }
